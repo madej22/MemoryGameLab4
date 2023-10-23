ecd26389c21c6b17ef60817cf89a985debd38509
madej22 <kacper.madejczyk@pollub.edu.pl>
2023-10-23 19:33:37 +0200

    Initial Commit

---
+++ MemoryGameLab4/MemoryGameLab4App.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameLab4App.swift
+//  MemoryGameLab4
+//
+//  Created by Kacper Madejczyk on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameLab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
